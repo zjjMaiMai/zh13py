66260bd5d066b7353f028fa2e45da7379419cd6b
zjjMaiMai <13913992+zjjMaiMai@users.noreply.github.com>
2023-01-13 18:51:43 +0800

    fix hdfs copy bug

--- hdfs/hdfs_copy.py
+++ hdfs/hdfs_copy.py
@@ -8,22 +8,20 @@ import tensorflow_io as tfio
 __all__ = ["hdfs_cp"]
 
 
-def hdfs_cp(src, dst, worker=16, cli=False):
-    if os.path.basename(src) == "":
-        src = os.path.split(src)[0]
-
-    if cli and tf.io.gfile.isdir(dst):
-        dst = tf.io.gfile.join(dst, os.path.basename(src))
-
+def hdfs_cp(src_list, dst, worker=16, cli=False):
     start_time = time.time()
     with concurrent.futures.ThreadPoolExecutor(max_workers=worker) as executor:
         tasks = []
-        if tf.io.gfile.isdir(src):
-            _copytree(src, dst, executor, tasks)
-        else:
-            _copy(src, dst, executor, tasks)
-
-        print(f"get [{len(tasks)}] jobs from {src} to {dst}!")
+        for src in src_list:
+            if os.path.basename(src) == "":
+                src = os.path.split(src)[0]
+            if cli and tf.io.gfile.isdir(dst):
+                dst_ = tf.io.gfile.join(dst, os.path.basename(src))
+            if tf.io.gfile.isdir(src):
+                _copytree(src, dst_, executor, tasks)
+            else:
+                _copy(src, dst_, executor, tasks)
+            print(f"get [{len(tasks)}] jobs from {src} to {dst_}!")
         concurrent.futures.wait(tasks)
     print(f"use time: {time.time() - start_time:.2f} sec")
     return dst
@@ -74,5 +72,4 @@ if __name__ == "__main__":
     parser.add_argument("-p", default=16, help="number of threads")
     args = parser.parse_args()
 
-    for src in args.SRC:
-        hdfs_cp(src, args.DST, args.p, True)
+    hdfs_cp(args.SRC, args.DST, args.p, True)
